596c210f9213f2ae6f837432bec2e431647e3cfe
BuildTools <unconfigured@null.spigotmc.org>
2023-03-13 00:59:11 -0500

    Adding start script for quilt

---
+++ minecraft/quilt/start.sh
@@ -0,0 +1,23 @@
+echo "Checking if {{SERVER_JARFILE}} exists"
+if [ ! (-f {{SERVER_JARFILE}} )]; then
+	echo "{{SERVER_JARFILE}} does not exist, attempting to install"
+fi
+
+
+echo "Checking if {{SERVER_JARFILE}} exists"
+if [ ! (-f {{SERVER_JARFILE}} )]; then
+	echo "{{SERVER_JARFILE}} does not exist, attempting to install"
+	if [!( -f quilt-installer-{{BUILD_NUMBER}}.jar )]; then
+		echo "Invalid install! Please reinstall your server!"
+		exit 1
+	fi
+	echo -e "Running java -jar quilt-installer-{{BUILD_NUMBER}}.jar install server {{MINECRAFT_VERSION}} --download-server"
+	java -jar quilt-installer-{{BUILD_NUMBER}}.jar install server {{MINECRAFT_VERSION}} --download-server
+
+	echo -e "Running mv quilt-installer-{{BUILD_NUMBER}}.jar {{SERVER_JARFILE}}"
+	mv quilt-installer-{{BUILD_NUMBER}}.jar {{SERVER_JARFILE}}
+
+	echo "Installtion succeeded, attempting to start the server!"
+fi
+
+java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar {{SERVER_JARFILE}}

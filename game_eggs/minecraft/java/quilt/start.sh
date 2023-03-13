350f92055ea4cb43e0bd49bdd2744fe089f0b81e
BuildTools <unconfigured@null.spigotmc.org>
2023-03-13 01:28:15 -0500

    .

--- game_eggs/minecraft/java/quilt/start.sh
+++ game_eggs/minecraft/java/quilt/start.sh
@@ -1,18 +1,18 @@
 echo "Checking if {{SERVER_JARFILE}} exists"
-if [ ! (-f {{SERVER_JARFILE}} )]; then
+if [ ! -f {{SERVER_JARFILE}} ]; then
 	echo "{{SERVER_JARFILE}} does not exist, attempting to install"
 fi
 
 
 echo "Checking if {{SERVER_JARFILE}} exists"
-if [ ! (-f {{SERVER_JARFILE}} )]; then
+if [ ! -f {{SERVER_JARFILE}} ]; then
 	echo "{{SERVER_JARFILE}} does not exist, attempting to install"
-	if [!( -f quilt-installer-{{BUILD_NUMBER}}.jar )]; then
+	if [!( -f quilt-installer.jar )]; then
 		echo "Invalid install! Please reinstall your server!"
 		exit 1
 	fi
 	echo -e "Running java -jar quilt-installer-{{BUILD_NUMBER}}.jar install server {{MINECRAFT_VERSION}} --download-server"
-	java -jar quilt-installer-{{BUILD_NUMBER}}.jar install server {{MINECRAFT_VERSION}} --download-server
+	java -jar quilt-installer.jar install server {{MINECRAFT_VERSION}} --download-server
 
 	echo -e "Running mv quilt-installer-{{BUILD_NUMBER}}.jar {{SERVER_JARFILE}}"
 	mv quilt-installer-{{BUILD_NUMBER}}.jar {{SERVER_JARFILE}}

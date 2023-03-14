186e639b74643a174810750d1740803fdf386e68
BuildTools <unconfigured@null.spigotmc.org>
2023-03-13 19:19:11 -0500

    Moved to QuiltMC api

--- game_eggs/minecraft/java/quilt/setup.sh
+++ game_eggs/minecraft/java/quilt/setup.sh
@@ -7,8 +7,8 @@ if [ -n "${DL_PATH}" ]; then
     echo -e "Using supplied download url: ${DL_PATH}"
     DOWNLOAD_URL=`eval echo $(echo ${DL_PATH} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 else
-    VER_EXISTS=`curl -s https://meta.fabricmc.net/v2/versions/game | jq -r --arg VERSION ${MINECRAFT_VERSION} '.[] | .version | contains($VERSION)' | grep -m1 true`
-    LATEST_VERSION=`curl -s https://meta.fabricmc.net/v2/versions/game | jq -r '[.[] | select(.stable == true)][0] | .version'`
+    VER_EXISTS=`curl -s https://meta.quiltmc.org/v3/versions/game | jq -r --arg VERSION ${MINECRAFT_VERSION} '[.[] | select(.stable == true)][] | .version | contains($VERSION)' | grep -m1 true`
+    LATEST_VERSION=`curl -s https://meta.quiltmc.org/v3/versions/game | jq -r '[.[] | select(.stable == true)][0] | .version'`
 
     if [ "${VER_EXISTS}" == "true" ]; then
         echo -e "Version is valid. Using version ${MINECRAFT_VERSION}"
@@ -17,13 +17,14 @@ else
         MINECRAFT_VERSION=${LATEST_VERSION}
     fi
 
-    BUILD_EXISTS=`curl -o /dev/null --silent -Iw '%{http_code}' https://maven.quiltmc.org/repository/release/org/quiltmc/quilt-installer/${BUILD_NUMBER}/quilt-installer-${BUILD_NUMBER}.jar`
+    BUILD_EXISTS=`curl -s https://meta.quiltmc.org/v3/versions/installer | jq -r --arg VERSION ${BUILD_NUMBER} '.[] | .version | contains($VERSION)' | grep -m1 true`
+    LATEST_BUILD=`curl -s https://meta.quiltmc.org/v3/versions/installer | jq -r '.[0] | .version '`
 
-    if [ "${BUILD_EXISTS}" == "200" ]; then
+    if [ "${BUILD_EXISTS}" == "true" ]; then
         echo -e "Build is valid for version using build ${BUILD_NUMBER}"
     else
         echo -e "Using the latest build for Quilt"
-        BUILD_NUMBER="latest"
+        BUILD_NUMBER=LATEST_BUILD
     fi
 
 
